066ffad9b7b2b69cb970d1df1c4451240939facf
neilkrichi <neilkrichi@gmail.com>
2017-07-18 16:13:24 -0700

    removeremoved tab bar

---
+++ homepage.js
@@ -0,0 +1,69 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Button,
+  Image,
+  StatusBar
+} from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    tabBarVisible: false,
+    tabBarLabel: 'Home',
+    tabBarIcon: () => (
+      <Image
+        source={require('./favicon.png')}
+        style={styles.icon}
+        />
+    ),
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Image source={require('./images/yosemite-merced.jpg')} style={styles.container} >
+        <View style={{flex: 1}}>
+          <Text style={{textAlign: 'center', marginTop: 30, fontSize: 20, backgroundColor: 'transparent', color: 'white'}}>Hi again, firas!</Text>
+        </View>
+      </Image>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: null,
+    height: null
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'transparent',
+  },
+  switchcam: {
+    backgroundColor: 'transparent',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+  }
+});
---
+++ notifications.js
@@ -0,0 +1,65 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Button,
+  Image,
+  StatusBar
+} from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+export default class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    tabBarVisible: false,
+    tabBarLabel: 'Notifications',
+    tabBarIcon: () => (
+      <Image
+        source={require('./favicon.png')}
+        style={styles.icon}
+        />
+    ),
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          style={styles.container}
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+          />
+        <FontAwesome name="rocket" size={30} color="#900" />
+        <Ionicon name="ios-reverse-camera" size={30} color="#900" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'transparent',
+  },
+  switchcam: {
+    backgroundColor: 'transparent',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
